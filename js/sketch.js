9f644f1789a2e991b20eeab0ad18aea32b7dd6c1
lArch1t3ct <167084066+lArch1t3ct@users.noreply.github.com>
2024-05-28 16:32:56 +0300

    Adding Trailing Effect
    A .mouser-over CSS class was added to the CSS file changing the background-color of the target
    element to yellow.
    
    In JS file, a event listener was added at the div level, the one that contains all the pixel divs.
    Using bubbling effect, we check which target has the 'pixel-div' and then we change its background
    color.

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,17 @@
 const divContainer = document.querySelector('.div-container');
+const GRID_DIV = 'pixel-div';
+const HOVER_EFFECT = 'mouse-over'
 
 for(let i = 0; i < 16; i++){
     for(let j = 0; j < 16; j++){
         const pixelDiv = document.createElement('div');
-        pixelDiv.classList.toggle('pixel-div');
+        pixelDiv.classList.toggle(GRID_DIV);
         divContainer.appendChild(pixelDiv);
     }
 }
+
+divContainer.addEventListener('mouseover', (event) => {
+    if(event.target.classList.contains(GRID_DIV)){ // classList returns a DOMTokenList
+        event.target.classList.add(HOVER_EFFECT);
+    }
+});
